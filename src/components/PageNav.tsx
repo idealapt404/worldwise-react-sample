5d5eded42ebafa9aa154ebbaeac9e733422771e6
Yoko Harada <yokolet@gmail.com>
2023-07-28 15:13:37 +0900

    updates pricing and product pages

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from './Logo.tsx';
 import styles from './PageNav.module.css';
 
 const PageNav: React.FC = () => {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import PageNav from '../components/PageNav.tsx';
+import styles from './Product.module.css';
 
 const Pricing: React.FC = () => {
   return (
-    <div>
+    <main className={styles.product}>
       <PageNav />
-      <h1>Pricing</h1>
-    </div>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
   );
 }
 
